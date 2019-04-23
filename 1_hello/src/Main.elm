60553f1d0a23871f04342f3ccefbbd947b818051
pedro <pedro.dvoracek@gmail.com>
2019-04-23 18:43:41 +0200

    asd

--- 1_hello/src/Main.elm
+++ 1_hello/src/Main.elm
@@ -5,9 +5,9 @@ import Html exposing (..)
 import Html.Attributes exposing (type_)
 import Html.Events exposing (..)
 
-
+main : Program () Model Msg{- deklaruje, co vraci funkce main -}
 main =
-    Browser.sandbox
+    Browser.sandbox {- sandbox je nejjednodussi, nicmene pouzivaji se i jine-}
         { init = init
         , update = update
         , view = view
@@ -16,26 +16,43 @@ main =
 
 type alias Model =
     { greetings : String
+    , users : List String
     }
 
 
 init : Model
 init =
     { greetings = "Hello World"
+    , users = ["Homer","lisa"]
     }
 
 
 type Msg
     = NoOp
+    | NewName String
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        NoOp ->
+            model
+             
+        NewName inputName ->
+            { model | users = inputName :: model.users }
+            
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text model.greetings ]
+        , input [onInput NewName ] []
+        , div [] 
+            (List.map viewUser model.users)
         ]
+
+viewUser : String -> Html Msg
+viewUser user = 
+    div [] [ text user ]
+
